4ebabd80fb3a369b604a1530c70ee7bab9c05856
taka <test>
2024-11-03 14:08:14 +0900

    テストケース追加

---
+++ swiftUI_XCUITest/Test.swift
@@ -0,0 +1,22 @@
+//
+//  Test.swift
+//  swiftUI_XCUITest
+//
+//  Created by mac on 2024/11/03.
+//
+
+
+class TestClass {
+    var item = 0
+    
+    func add(num: Int) -> Int {
+        return num + 1
+        
+    }
+    
+    func minus(num: Int) -> Int {
+        return num - 1
+        
+    }
+    
+}
